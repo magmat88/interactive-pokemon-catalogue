0141165e2570b0eb6cab2131c1705fa6c9ecd6fe
magmat88 <magdalena.matusiak88@gmail.com>
2022-03-09 16:03:54 +0100

    Changed request to api in redux action

--- src/actions/getPokemonList.ts
+++ src/actions/getPokemonList.ts
@@ -2,9 +2,12 @@ import axios from 'axios';
 import { TYPE_GET_POKEMON_LIST } from '../config/actionTypes';
 
 export function getPokemonList(listUrl: string) {
-  const request = axios.get(listUrl);
+  const request = axios.get(listUrl).then((response) => {
+    return response.data;
+  });
   return {
     type: TYPE_GET_POKEMON_LIST,
     payload: request,
   };
 }
+
