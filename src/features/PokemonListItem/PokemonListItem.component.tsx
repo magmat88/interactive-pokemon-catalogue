5c153abda5ea16b6b5039ba6a4701a1853d67c62
magmat88 <magdalena.matusiak88@gmail.com>
2022-03-12 21:57:17 +0100

    Refactored

--- src/features/PokemonListItem/PokemonListItem.component.tsx
+++ src/features/PokemonListItem/PokemonListItem.component.tsx
@@ -30,44 +30,56 @@ export function PokemonListItem({ pokemon }: any): JSX.Element {
     userInput,
     pokemonsWithVisibleDetails,
   ]);
-  return (
-    <>
-      {filterPokemonByType(filterByType, renderPokemonTypes(pokemon)) &&
-        filterPokemonByName(filterByName, pokemon.name) && (
-          <div
-            className="pokemonList__listItem--visible pokemonListItem"
-            // onClick={togglePokemonDetailsVisibility(pokemon.name)}
-          >
-            <h1 className="pokemonListItem__text--label">{pokemon.name}</h1>
-            <figure className="pokemonListItem__figure">
-              <img
-                className="pokemonListItem__img--small"
-                src={`${pokemon.sprites['front_default']}`}
-                alt={`${pokemon.name}`}
-              />
-              <figcaption className="pokemonListItem__figcaption">
-                {renderPokemonTypes(pokemon)?.map((type: any) => {
-                  return (
-                    <div className="pokemonListItem__figcaptionItem" key={type}>
-                      <p>{type}</p>
-                    </div>
-                  );
-                })}
-              </figcaption>
-            </figure>
-            <article
-              className={setListItemVisibilityClass(
-                pokemonsWithVisibleDetails,
-                pokemon.name
-              )}
-            >
-              <div>
-                <p>Height: {pokemon.height} m</p>
-                <p>Weight: {pokemon.weight} kg</p>
-              </div>
-            </article>
-          </div>
-        )}
-    </>
+
+  const filteredByType = filterPokemonByType(
+    filterByType,
+    renderPokemonTypes(pokemon)
   );
+  const filteredByName = filterPokemonByName(filterByName, pokemon.name);
+
+  function PokemonItemDetails(): JSX.Element {
+    return (
+      <article
+        // className={setListItemVisibilityClass(
+        //   pokemonsWithVisibleDetails,
+        //   pokemon.name
+        // )}
+      >
+        <div>
+          <p>Height: {pokemon.height} m</p>
+          <p>Weight: {pokemon.weight} kg</p>
+        </div>
+      </article>
+    );
+  }
+
+  function PokemonItemTile(): JSX.Element {
+    return (
+      <div
+        className="pokemonList__listItem--visible pokemonListItem"
+        // onClick={togglePokemonDetailsVisibility(pokemon.name)}
+      >
+        <h1 className="pokemonListItem__text--label">{pokemon.name}</h1>
+        <figure className="pokemonListItem__figure">
+          <img
+            className="pokemonListItem__img--small"
+            src={`${pokemon.sprites['front_default']}`}
+            alt={`${pokemon.name}`}
+          />
+          <figcaption className="pokemonListItem__figcaption">
+            {renderPokemonTypes(pokemon)?.map((type: any) => {
+              return (
+                <div className="pokemonListItem__figcaptionItem" key={type}>
+                  <p>{type}</p>
+                </div>
+              );
+            })}
+          </figcaption>
+        </figure>
+        <PokemonItemDetails />
+      </div>
+    );
+  }
+
+  return <>{filteredByType && filteredByName && <PokemonItemTile />}</>;
 }
--- src/features/pokemonAppSlice.ts
+++ src/features/pokemonAppSlice.ts
@@ -1,64 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../store';
-import {
-  // POKEMON_APP__ADD_POKEMON_WITH_VISIBLE_DETAILS,
-  // POKEMON_APP__CHANGE_POKEMON_NAME_FILTER,
-  // POKEMON_APP__CHANGE_POKEMON_TYPE_FILTER,
-  // POKEMON_APP__REMOVE_POKEMON_WITH_VISIBLE_DETAILS,
-  // POKEMON_APP__CHANGE_APP_THEME,
-  // POKEMON_APP__CHANGE_LIMIT,
-  // POKEMON_APP__CHANGE_OFFSET,
-} from '../config/actionTypes';
+import {} from '../config/actionTypes';
 import { APP_THEME__LIGHT, APP_THEME__DARK } from '../config/constants';
-
-// export function removePokemonWithVisibleDetails(pokemonName: string) {
-//   return {
-//     type: POKEMON_APP__REMOVE_POKEMON_WITH_VISIBLE_DETAILS,
-//     payload: pokemonName,
-//   };
-// }
-
-// export function addPokemonWithVisibleDetails(pokemonName: string) {
-//   return {
-//     type: POKEMON_APP__ADD_POKEMON_WITH_VISIBLE_DETAILS,
-//     payload: pokemonName,
-//   };
-// }
-
-// export function changePokemonNameFilter(filterByName: any) {
-//   return {
-//     type: POKEMON_APP__CHANGE_POKEMON_NAME_FILTER,
-//     payload: filterByName,
-//   };
-// }
-
-// export function changePokemonTypeFilter(filterByType: any) {
-//   return {
-//     type: POKEMON_APP__CHANGE_POKEMON_TYPE_FILTER,
-//     payload: filterByType,
-//   };
-// }
-
-// export function changeAppTheme(selectedTheme: any) {
-//   return {
-//     type: POKEMON_APP__CHANGE_APP_THEME,
-//     payload: selectedTheme,
-//   };
-// }
-
-// export function changeLimit(limit: any) {
-//   return {
-//     type: POKEMON_APP__CHANGE_LIMIT,
-//     payload: limit,
-//   };
-// }
-
-// export function changeOffset(offset: any) {
-//   return {
-//     type: POKEMON_APP__CHANGE_OFFSET,
-//     payload: offset,
-//   };
-// }
 
 const pokemonAppInitState: any = {
   pokemonsWithVisibleDetails: [],
@@ -81,10 +24,10 @@ export const pokemonAppSlice = createSlice({
       state.userInput = action.payload;
     },
 
-    addPokemonWithVisibleDetails: (state, action: any) => { 
-        state.pokemonsWithVisibleDetails = [
-          ...state.pokemonsWithVisibleDetails.concat(
-            action.payload)]
+    addPokemonWithVisibleDetails: (state, action: any) => {
+      state.pokemonsWithVisibleDetails = [
+        ...state.pokemonsWithVisibleDetails.concat(action.payload),
+      ];
     },
 
     removePokemonWithVisibleDetails: (state, action: any) => {
@@ -93,23 +36,23 @@ export const pokemonAppSlice = createSlice({
       );
       if (index !== -1) {
         state.pokemonsWithVisibleDetails = [
-            ...state.pokemonsWithVisibleDetails.slice(0, index),
-            ...state.pokemonsWithVisibleDetails.slice(index + 1),
-          ];
-          return state;
-        };
-        // index !== -1 && {
-        //   ...state,
-        //   pokemonsWithVisibleDetails: [
-        //     ...state.pokemonsWithVisibleDetails.slice(0, index),
-        //     ...state.pokemonsWithVisibleDetails.slice(index + 1),
-        //   ],
-        // }
-      
+          ...state.pokemonsWithVisibleDetails.slice(0, index),
+          ...state.pokemonsWithVisibleDetails.slice(index + 1),
+        ];
+        return state;
+      }
+      // index !== -1 && {
+      //   ...state,
+      //   pokemonsWithVisibleDetails: [
+      //     ...state.pokemonsWithVisibleDetails.slice(0, index),
+      //     ...state.pokemonsWithVisibleDetails.slice(index + 1),
+      //   ],
+      // }
     },
 
     changeAppTheme: (state, action: any) => {
-      state.appTheme = state.appTheme === action.payload ? state.appTheme : action.payload;
+      state.appTheme =
+        state.appTheme === action.payload ? state.appTheme : action.payload;
 
       // return {
       //   ...state,
@@ -159,15 +102,14 @@ export const selectLimit = (state: RootState) => state.pokemonApp.limit;
 export const selectOffset = (state: RootState) => state.pokemonApp.offset;
 export const selectUserInput = (state: RootState) => state.pokemonApp.userInput;
 
-
 export const {
   addPokemonWithVisibleDetails,
-  changePokemonNameFilter,
-  changePokemonTypeFilter,
-  removePokemonWithVisibleDetails,
   changeAppTheme,
   changeLimit,
   changeOffset,
+  changePokemonNameFilter,
+  changePokemonTypeFilter,
   changeUserInput,
+  removePokemonWithVisibleDetails,
 } = pokemonAppSlice.actions;
 export default pokemonAppSlice.reducer;
